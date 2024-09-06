524d96dba63f825c1f9f0d428424beb173d83a7a
Rafael-AIDev <rafael.it.ai.br@gmail.com>
2024-09-06 15:25:22 -0300

    continuação

---
+++ a34_13p.py
@@ -0,0 +1,20 @@
+import pyautogui as auto
+import time
+
+auto.PAUSE = 0.5
+
+auto.press('win')
+auto.write('firefox')
+auto.press('enter')
+auto.hotkey('alt','space')
+auto.press('x')
+auto.write('youtube.com.br')
+auto.press('enter')
+
+time.sleep(2)
+
+for i in range(4):
+    auto.press('tab')
+
+auto.write('Python')
+auto.press('enter')
